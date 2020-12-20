8fa7777653709e4517159dc82676d636a4c98f03
Mostafa Alavi <63903935+Mostafa-Alavi@users.noreply.github.com>
2020-12-20 10:06:49 +0330

    fix answer scoring bug

--- scales/YBOCS9A.py
+++ scales/YBOCS9A.py
@@ -1,25 +1,29 @@
-factors = {
-    1 :'Aggressive_Obsession' , 2 :'Aggressive_Obsession' , 3 :'Aggressive_Obsession' , 4 :'Aggressive_Obsession' , 5 :'Aggressive_Obsession' , 6 :'Aggressive_Obsession' ,  7:'Aggressive_Obsession' , 8 :'Aggressive_Obsession' , 9 :'Aggressive_Obsession',
-    10 :'Cleanliness_Obsession' , 11 :'Cleanliness_Obsession' , 12 :'Cleanliness_Obsession' , 13 :'Cleanliness_Obsession' , 14 :'Cleanliness_Obsession' , 15 :'Cleanliness_Obsession' , 16 :'Cleanliness_Obsession' , 17:'Cleanliness_Obsession',
-    18 :'Sexual_Obsession' , 19 :'Sexual_Obsession' , 20 :'Sexual_Obsession' , 21 :'Sexual_Obsession', 
-    22 :'Hoarding_Obsession' ,
-    23 :'Religious_Obsession' , 24 :'Religious_Obsession' ,
-    25 :'Symmetry-Needed_Obsession' ,
-    26 :'Sundry_Obsession' , 27 :'Sundry_Obsession',  28:'Sundry_Obsession' , 29 :'Sundry_Obsession' , 30 :'Sundry_Obsession' , 31 :'Sundry_Obsession' , 32 :'Sundry_Obsession' , 33 :'Sundry_Obsession' , 34 :'Sundry_Obsession' , 35 :'Sundry_Obsession' ,
-    36 :'Physical_Obsession' , 37 :'Physical_Obsession' ,
-    38 :'Washing_coercion' , 39 :'Washing_coercion' , 40 :'Washing_coercion' , 41 :'Washing_coercion' ,
-    42 :'Checking_coercion' , 43 :'Checking_coercion' , 44 :'Checking_coercion' , 45 :'Checking_coercion' , 46 :'Checking_coercion',
-    47 :'Repeating_coercion', 48 :'Repeating_coercion',  
-    49 :'Counting_coercion' , 
-    50 :'Discipline_coercion' , 
-    51 :'Hoarding_coercion' , 
-    52 :'Sundry_coercion' , 53 :'Sundry_coercion', 54 :'Sundry_coercion' , 55 :'Sundry_coercion' , 56 :'Sundry_coercion' , 57 :'Sundry_coercion' , 58 :'Sundry_coercion',
-    59 : 'Attachment2', 60 : 'Attachment2', 61 : 'Attachment2',62 : 'Attachment2',63 : 'Attachment2',64 : 'Attachment2',65 : 'Attachment2', 66 : 'Attachment2',67 : 'Attachment2',68 : 'Attachment2',69 : 'Attachment2', 70 : 'Attachment2',71 : 'Attachment2',72 : 'Attachment2'
-    }
+from Data import Data
+import scales.dictionary.YBOCS9A as dictionary
+
+class YBOCS9A(Data):
+    scores = {'raw':None }
+ 
+    def scoring_raw(self, score):      
+      
+        
+        score.set(dictionary.factors_name,0)
+        
+        for i, item in self.items():
+            try:
+                answer = int(item.get('user_answered'))-1
+                factor = dictionary.factors[i + 1]
+                score.increase(factor , answer ) if factor else None
+            except:
+                pass
+    
+    
 
 
-factors_name = ('Aggressive_Obsession' ,'Cleanliness_Obsession' , 'Sexual_Obsession' ,'Hoarding_Obsession','Religious_Obsession','Symmetry-Needed_Obsession','Physical_Obsession','Sundry_Obsession',
-                'Washing_coercion' , 'Checking_coercion', 'Repeating_coercion','Counting_coercion','Discipline_coercion','Hoarding_coercion' ,'Sundry_coercion' , 'Attachment2')
+    
+       
+    
+        
+        
 
-
-
+    
