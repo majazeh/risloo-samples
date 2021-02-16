278b1d088d436477f9356c74637f609bd076d57f
Mostafa Alavi <63903935+Mostafa-Alavi@users.noreply.github.com>
2021-02-16 17:11:34 +0330

    add interpreting part

--- unilities.py
+++ unilities.py
@@ -1,9 +1,10 @@
 import argparse
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("scale", help="scale id [MBTI93, MBTI9A, Raven93, ...]")
-    parser.add_argument("-it", "--input_type", help="tell what's input_type [raw,local,remote]", default='raw')
+    parser.add_argument("-s","--scale" ,help="scale id [MBTI93, MBTI9A, Raven93, ...]")
+    parser.add_argument("-it", "--input_type", help="tell what's input_type [raw,file,remote]", default='raw')
     parser.add_argument("-id", "--input_data", help="absolute addrress of where you want to read", required=True)
     parser.add_argument("-ot", "--output_type", help="absolute addrress of where you want to read", default='raw')
+    parser.add_argument("-ip", "--Interpretation", help="Do you want to run interpretation?", action="store_true")
     parser.add_argument("-u", "--unittest", help="performing unittest of this test", action="store_true")
     return dict(parser.parse_args().__dict__.items())
