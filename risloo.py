ab5d71a4ad30a511da1622efa7c673d7aadf0c2f
mostafa alavi <mostafa.alavi25@gmail.com>
2021-03-31 23:28:54 +0430

    add excell exporting if is needed

--- risloo.py
+++ risloo.py
@@ -41,7 +41,36 @@ class risloo():
         
         if self.args['Interpretation']:
             sys.stdout.write(self.interpreting_scale.interpret.get_text() + "\n")
+    
+    def export_excell(self):
+        my_path = '/home/mostafa/Majazeh/tests/scoring/forms/'
+        addr = self.args['input_data'].split('.json')[0]
+        num = addr[-1]
+        names = addr.split('/')
+        
+        test_name = names [-2]
+        file_name = names [-1].split('_')[0]
+        
+        
+        excell_path = my_path + '/' + test_name + '/' + file_name +'_test_' + num + '.xlsx'
+        book = openpyxl.load_workbook(excell_path )
+        
+        
+        book.create_sheet("outputs")
+        sheet = book["outputs"]
+        sheet.cell(row = 1, column=2).value = 'پاسخ سیستم'
+        sheet.cell(row = 1, column=3).value = 'پاسخ دستی کارشناس'
+        
+        out_dic = self.scoring_scale.score.toDict()
+        for i,key in enumerate(out_dic.keys()):
+            out_dic[key]
+            sheet.cell(row= i+2, column=1).value = key
+            sheet.cell(row= i+2, column=2).value = out_dic[key]
+        
+        book.save(excell_path)
+
         
 if (__name__ == '__main__'):
     rsl = risloo(**unilities.get_args())
+    # rsl.export_excell()
     rsl.export()
