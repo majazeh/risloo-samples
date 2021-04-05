2b9e2822a1b680d177bd56db33d0ae42cfa8a1bf
mostafa alavi <mostafa.alavi25@gmail.com>
2021-04-05 16:12:13 +0430

    some changes about file names

--- scoring/JSGS-93.py
+++ scoring/JSGS-93.py
@@ -1,7 +1,7 @@
 from Data import Data
-import scoring.dictionary.PSDI as dictionary
+import scoring.dictionary.JSGS as dictionary
 
-class PSDI(Data):
+class JSGS(Data):
     scores = {'raw' :  None }# 
     
     def scoring_raw(self, score):
@@ -10,7 +10,7 @@ class PSDI(Data):
                 answer = int(item.get('user_answered')) 
                 factors = dictionary.factors[i + 1]
                 for factor in factors:
-                    score.increase(factor , answer ) if factor else None
+                    score.increase(factor , answer ) 
             except:
                 pass
         
