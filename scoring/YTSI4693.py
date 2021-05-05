15d3066bd6d9836eca14b7708f5a5aaac76366e7
mostafa alavi <mostafa.alavi25@gmail.com>
2021-05-05 14:49:52 +0430

    renaming someof  file names

--- scoring/YTSI4693.py
+++ scoring/YTSI4693.py
@@ -1,7 +1,7 @@
 from Data import Data
-import scoring.dictionary.YTSI93 as dictionary
+import scoring.dictionary.YTSI4693 as dictionary
 
-class YTSI93(Data):
+class YTSI4693(Data):
     scores = {'raw' :  None }# 
     
     def scoring_raw(self, score):
