928f4ed97924ed8bf03fb7cc009b7d11d8a67710
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-05-12 18:57:33 +0430

    change YCI test with the newer version

--- tester.sh
+++ tester.sh
@@ -1,6 +1,6 @@
 
 # if NOT saving => enumerous tests
-python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name JRAQ --file_name JRAQ93 --test_numbers 100 
+# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name JRAQ --file_name JRAQ93 --test_numbers 100 
 
 # just saving for scientific tests
-# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name JRAQ --file_name JRAQ93 --test_numbers 2 -s
+python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name YCI --file_name YCI93 --test_numbers 2 -s
