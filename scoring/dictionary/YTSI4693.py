6f10f5313211f703f1877d375c2b896421d43a3a
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-05-12 19:06:43 +0430

    fix a bug

--- scoring/dictionary/YTSI4693.py
+++ scoring/dictionary/YTSI4693.py
@@ -15,7 +15,7 @@ f14 = 'excessive_self_sercrifice'
 f15 = 'emotional_inhibition'
 
 
-factors_names = (f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15,f16,f17)
+factors_names = (f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13,f14,f15)
 factors = {
     1 :(f1,)
     , 2 :(f1,)
