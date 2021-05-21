c96782407ab8c853c488c39749f3c2292bd42536
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-05-21 11:26:37 +0430

    mix test producing and test scoring in a script

--- tester.sh
+++ tester.sh
@@ -1,6 +1,10 @@
 
 # if NOT saving => enumerous tests
-# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name JRAQ --file_name JRAQ93 --test_numbers 100 
+# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name CRAAS --file_name CRAAS93 --test_numbers 100 
 
 # just saving for scientific tests
-python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name YCI --file_name YCI93 --test_numbers 2 -s
+# test producing:
+python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name CRAAS --file_name CRAAS93 --test_numbers 2 -s
+# test scoring
+python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s CRAAS93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/CRAAS/CRAAS93_test_1.json -ot raw json excell
+python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s CRAAS93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/CRAAS/CRAAS93_test_2.json -ot raw json excell
