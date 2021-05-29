8443dbd8cc1b11e1fa85b74d77375efc09b346b6
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-05-29 10:58:27 +0430

    fix JSGS93 bug

--- scoring/JSGS93.py
+++ scoring/JSGS93.py
@@ -5,12 +5,15 @@ class JSGS93(Data):
     scores = {'raw' :  None }# 
     
     def scoring_raw(self, score):
-        score.set(dictionary.factors_names,0)
+        score.set(dictionary.factor_names,0)
         for i, item in self.items():   
             try:
                 answer = int(item.get('user_answered')) 
+
                 factors = dictionary.factors[i + 1]
+                
                 for factor in factors:
+                    score.increase('raw',  answer )
                     score.increase(factor , answer ) 
             except:
                 pass
