fad0748e7c577ca6cafd6f33ad49d61a79a2a80e
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-06-12 17:07:43 +0430

    add EPQC93 and EPQA93 tests

---
+++ scoring/EPQC93.py
@@ -0,0 +1,24 @@
+from Data import Data
+import scoring.dictionary.EPQC93 as dictionary
+
+class EPQC93(Data):
+    scores = {'raw' :  None }# 
+    
+    def scoring_raw(self, score):
+        score.set(dictionary.factors_names,0)
+        for i, item in self.items():   
+            try:
+                answer = int(item.get('user_answered')) 
+                factors_dic = dictionary.factors[answer]
+                
+                
+                if i + 1 in factors_dic.keys():
+                    factors = factors_dic [i+1]
+                    
+
+                    for factor in factors:
+                        score.increase(factor , weight = 1 )
+            except:
+                pass
+        
+     
--- tester.sh
+++ tester.sh
@@ -1,13 +1,13 @@
 
 ## if NOT saving => enumerous tests
 
-# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name JSGS --file_name JSGS93 --test_numbers 100 
+# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name EPQA --file_name EPQA93 --test_numbers 100 
 
 
 ## just saving for scientific tests
 
 # test producing:
-# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name RCMAS --file_name RCMAS93 --test_numbers 2 -s
+python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name EPQA --file_name EPQA93 --test_numbers 2 -s
 # test scoring
-python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s JSGS93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/RCMAS/RCMAS93_test_1.json -ot raw json excell
-python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s RCMAS93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/RCMAS/RCMAS93_test_2.json -ot raw json excell
+python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s EPQA93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/EPQA/EPQA93_test_1.json -ot raw json excell
+python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s EPQA93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/EPQA/EPQA93_test_2.json -ot raw json excell
