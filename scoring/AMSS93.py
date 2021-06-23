cd626b94bf1b9d9fb47855d8f3e811dda5651bc2
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-06-23 12:59:15 +0430

    Add AMSS and AMSC tests

---
+++ scoring/AMSS93.py
@@ -0,0 +1,24 @@
+from Data import Data
+import scoring.dictionary.AMS93 as dictionary
+
+class AMSS93(Data):
+    scores = {'raw' :  None }# 
+    
+    def scoring_raw(self, score):
+        score.set(dictionary.factors_names,0)
+        option_numbers = dictionary.option_numbers
+        for i, item in self.items():   
+            try:
+                answer = int(item.get('user_answered')) 
+                factors = dictionary.factors[i + 1]
+                
+                score.increase('raw' , answer )
+                for factor in factors:
+                        
+                    score.increase(factor , answer )    
+            
+           
+            except:
+                pass
+        
+        
---
+++ scoring/dictionary/AMS93.py
@@ -0,0 +1,49 @@
+f1 = 'intrinsic_motivation_to_know'
+f2 = 'intrinsic_motivation_toward_accomplishment'
+f3 = 'intrinsic_motivation_to_experience_stimulation'
+f4 = 'extrinsic_motivation_identified'
+f5 = 'extrinsic_motivation_introjected'
+f6 = 'extrinsic_motivation_external_regulation'
+f7 = 'unmotivation'
+
+option_numbers = 7
+
+factors_names = ('raw',f1,f2,f3,f4,f5,f6,f7)
+
+factors = {
+    2 :(f1,)
+    , 9 :(f1,)
+    , 16 :(f1,)
+    , 23 :(f1,)
+    
+    , 6 :(f2,) 
+    , 13 :(f2,)
+    , 20 :(f2,)
+    , 27 :(f2,)
+    
+    , 4 :(f3,)
+    , 11 :(f3,)
+    , 18 :(f3,)
+    , 25 :(f3,)
+    
+    , 3 :(f4,)
+    , 10 :(f4,)
+    , 17 :(f4,)
+    , 24 :(f4,)
+    
+    , 7 :(f5,)
+    , 14 :(f5,)
+    , 21 :(f5,)
+    , 28 :(f5,)
+    
+    , 1 :(f6,)
+    , 8 :(f6,)
+    , 15 :(f6,)
+    , 22 :(f4,)
+    
+    , 5 :(f7,)
+    , 12 :(f7,)
+    , 19 :(f7,)
+    , 26:(f7,)
+    
+}
