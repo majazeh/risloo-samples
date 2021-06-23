da0b2ba330f86671173bb69f27e3048c6ffc8faa
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-06-23 14:04:56 +0430

    Add CERQS93 test

---
+++ scoring/dictionary/CERQS93.py
@@ -0,0 +1,44 @@
+f1 = 'self_blame'
+f2 = 'acceptance'
+f3 = 'rumination'
+f4 = 'positive_refocusing'
+f5 = 'refocusing_on_planing'
+f6 = 'positive_reappraisal'
+f7 = 'putting_to_perspective'
+f8 = 'catastrophizing'
+f9 = 'other_blame'
+
+option_numbers = 5
+
+factors_names = ('raw',f1,f2,f3,f4,f5,f6,f7,f8,f9)
+
+factors = {
+    4 :(f1,)
+    , 14 :(f1,)
+    
+    , 5 :(f2,)
+    , 1 :(f2,)
+    
+    , 6 :(f3,) 
+    , 2 :(f3,)
+    
+    , 11 :(f4,)
+    , 7 :(f4,)
+    
+    , 15 :(f5,)
+    , 12 :(f5,)
+    
+    , 8 :(f6,)
+    , 3 :(f6,)
+    
+    , 16 :(f7,)
+    , 13 :(f7,)
+    
+    , 17 :(f8,)
+    , 9 :(f8,)
+    
+    , 18 :(f9,)
+    , 10 :(f9,)
+    
+    
+}
