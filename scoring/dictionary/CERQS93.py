f13ce4978c5113908946a73574e79b61e62123e7
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-06-25 23:28:58 +0430

    fix CERQS test bug

--- scoring/dictionary/CERQS93.py
+++ scoring/dictionary/CERQS93.py
@@ -7,38 +7,41 @@ f6 = 'positive_reappraisal'
 f7 = 'putting_to_perspective'
 f8 = 'catastrophizing'
 f9 = 'other_blame'
+f10 = 'adaptive_regulation'
+f11 = 'unadaptive_reglation'
+
 
 option_numbers = 5
 
-factors_names = ('raw',f1,f2,f3,f4,f5,f6,f7,f8,f9)
+factors_names = (f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11)
 
 factors = {
-    4 :(f1,)
-    , 14 :(f1,)
+    4 :(f1,f11)
+    , 14 :(f1,f11)
     
-    , 5 :(f2,)
-    , 1 :(f2,)
+    , 5 :(f2,f10)
+    , 1 :(f2,f10)
     
-    , 6 :(f3,) 
-    , 2 :(f3,)
+    , 6 :(f3,f11) 
+    , 2 :(f3,f11)
     
-    , 11 :(f4,)
-    , 7 :(f4,)
+    , 11 :(f4,f10)
+    , 7 :(f4,f10)
     
-    , 15 :(f5,)
-    , 12 :(f5,)
+    , 15 :(f5,f10)
+    , 12 :(f5,f10)
     
-    , 8 :(f6,)
-    , 3 :(f6,)
+    , 8 :(f6,f10)
+    , 3 :(f6,f10)
     
-    , 16 :(f7,)
-    , 13 :(f7,)
+    , 16 :(f7,f10)
+    , 13 :(f7,f10)
     
-    , 17 :(f8,)
-    , 9 :(f8,)
+    , 17 :(f8,f11)
+    , 9 :(f8,f11)
     
-    , 18 :(f9,)
-    , 10 :(f9,)
+    , 18 :(f9,f11)
+    , 10 :(f9,f11)
     
     
 }
