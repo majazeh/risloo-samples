702c59cdb2a4aa0c113646a59bbb2a22a1a278b6
Mostafa-Alavi <mostafa.alavi25@gmail.com>
2021-07-09 01:21:04 +0430

    add SAM93 test

---
+++ scoring/SAM93.py
@@ -0,0 +1,43 @@
+from Data import Data
+import scoring.dictionary.SAM93 as dictionary
+
+class SAM9A(Data):
+    scores = {'raw' :  None }# 
+    
+    def scoring_raw(self, score):
+        score.set(dictionary.factors_names,0)
+        option_numbers = dictionary.option_numbers
+        for i, item in self.items():   
+            try:
+                answer = int(item.get('user_answered')) 
+                factors = dictionary.factors[i + 1]
+                
+                score.increase('raw' , answer-1 )
+                for factor in factors:
+                        
+                    score.increase(factor , answer-1 )    
+            
+           
+            except:
+                pass
+        
+        raw_score = score.get('raw')
+        interpretation = self.get_level_interpretation(raw_score)
+        score.set('interpretation_level', interpretation[0])
+        score.set('percentage_interpretation', interpretation[1])
+
+
+    def get_level_interpretation(self,raw_score):
+        
+        intervals = list(dictionary.level_interpretation.keys())
+        
+        for interval in intervals:
+            
+            if interval[0] <= raw_score <= interval[1]:
+                return dictionary.level_interpretation[interval]
+
+        else:
+            return None
+        
+        
+        
---
+++ scoring/dictionary/SAM93.py
@@ -0,0 +1,45 @@
+f1 = 'conflicting_thoughts_of_self'
+f2 = 'dichotomous_view_of_self'
+f3 = 'fear_of_others_judgment'
+f4 = 'preoccupation_with_self_worth'
+
+
+option_numbers = 5
+
+factors_names = ('raw',f1,f2,f3,f4)
+
+factors = {
+    4 :(f1,)
+    , 7 :(f1,)
+    , 8 :(f1,)
+    , 11 :(f1,)
+    , 12 :(f1,)
+    , 16 :(f1,)
+    , 17 :(f1,)
+    
+
+    , 3 :(f2,) 
+    , 9 :(f2,)
+    , 13 :(f2,)
+    , 14 :(f2,)
+    
+    , 1 :(f3,)
+    , 2 :(f3,)
+    , 6 :(f3,)
+    , 18 :(f3,)
+    
+    , 5 :(f4,)
+    , 10 :(f4,)
+    , 15 :(f4,)
+    , 19 :(f4,)
+    
+    
+}
+
+level_interpretation = {
+    (0,13):  ('very_desirable' , "0%_20%")
+    ,(14,25): ('desirable' , "20%_40%")
+    ,(26,37) : ('moderate' , "40%_60%")
+    ,(38,49) : ('undesirable' , "60%_80%")
+    ,(50,59) : ('very_undesirable' ,"80%_100%")
+}
