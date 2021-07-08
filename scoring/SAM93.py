6216e820d520958dc0d3662e53a3a37959e51889
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-07-09 01:41:34 +0430

    add SAM93 test files

--- scoring/SAM93.py
+++ scoring/SAM93.py
@@ -1,14 +1,16 @@
 from Data import Data
 import scoring.dictionary.SAM93 as dictionary
 
-class SAM9A(Data):
+class SAM93(Data):
     scores = {'raw' :  None }# 
     
     def scoring_raw(self, score):
         score.set(dictionary.factors_names,0)
         option_numbers = dictionary.option_numbers
-        for i, item in self.items():   
+        for i, item in self.items(): 
+              
             try:
+                
                 answer = int(item.get('user_answered')) 
                 factors = dictionary.factors[i + 1]
                 
--- tester.sh
+++ tester.sh
@@ -1,13 +1,13 @@
 
 ## if NOT saving => enumerous tests
 
-# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name CERQS --file_name CERQS93 --test_numbers 100 
+# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name SAM --file_name SAM93 --test_numbers 100 
 
 
 ## just saving for scientific tests
 
 # test producing:
-# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name CERQS --file_name CERQS93 --test_numbers 2 -s
+python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name SAM --file_name SAM93 --test_numbers 2 -s
 # test scoring
-python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s AMSC93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/AMS/AMSC93_test_1.json -ot raw json excell
-python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s AMSC93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/AMS/AMSC93_test_2.json -ot raw json excell
+python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s SAM93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/SAM/SAM93_test_1.json -ot raw json excell
+python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s SAM93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/SAM/SAM93_test_2.json -ot raw json excell
