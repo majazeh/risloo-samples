24475c0b77e704fe6141a706639029c7ea5fb083
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-10-02 10:29:23 +0330

    add STAIY93 test

--- tester.sh
+++ tester.sh
@@ -1,13 +1,13 @@
 
 ## if NOT saving => enumerous tests
 
-#python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name IUS --file_name IUS93 --test_numbers 200 
+# python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name STAIY --file_name STAIY93 --test_numbers 200 
 
 
 ## just saving for scientific tests
 
 # test producing:
-python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name IUS --file_name IUS93 --test_numbers 2 -s
+python3 /home/mostafa/Majazeh/risloo-samples/tester.py --folder_name STAIY --file_name STAIY93 --test_numbers 2 -s
 # test scoring
-python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s IUS93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/IUS/IUS93_test_1.json -ot raw json excell
-python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s IUS93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/IUS/IUS93_test_2.json -ot raw json excell
+python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s STAIY93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/STAIY/STAIY93_test_1.json -ot raw json excell
+python3 /home/mostafa/Majazeh/risloo-samples/risloo.py -s STAIY93 -it file -id /home/mostafa/Majazeh/risloo-samples/scoring/tests/STAIY/STAIY93_test_2.json -ot raw json excell
