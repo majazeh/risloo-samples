6ee6b0d07b8cd316f3599d3cca945ed1a4028dea
Mostafa Alavi <mostafa.alavi25@gmail.com>
2021-10-02 10:49:44 +0330

    add SDCAQ93 test

---
+++ scoring/SDCAQ93.py
@@ -0,0 +1,20 @@
+from Data import Data
+import scoring.dictionary.SDCAQ93 as dictionary
+
+class SDCAQ93(Data):
+    scores = {'raw' :  None }# 
+    
+    def scoring_raw(self, score):
+        score.set(dictionary.factors_names,0)
+        option_numbers = dictionary.option_numbers
+        for i, item in self.items():   
+            try:
+                answer = int(item.get('user_answered')) 
+                factors = dictionary.factors[i + 1]     
+                score.increase('raw' , answer )
+                for factor in factors:     
+                    score.increase(factor , answer )    
+            except:
+                pass
+        
+        
---
+++ scoring/dictionary/SDCAQ93.py
@@ -0,0 +1,41 @@
+f1 = 'thought_suppression'
+f2 = 'thought_substitution'
+f3 = 'distraction_subscale'
+f4 = 'avoidance_of_threatening_stimuli'
+f5 = 'transformation_of_images_into_thoughts'
+
+option_numbers = 5
+
+factors_names = ('raw',f1,f2, f3, f4, f5)
+
+factors = {
+    1: (f1,)
+    , 2: (f1,)
+    , 5: (f1,)
+    , 6: (f1,)
+    , 14: (f1,) 
+    
+    , 4: (f2,)
+    , 11: (f2,)
+    , 17: (f2,)
+    , 20: (f2,)
+    , 25: (f2,)
+    
+    , 8: (f3,)
+    , 10: (f3,)
+    , 12: (f3,)
+    , 13: (f3,)
+    , 21: (f3,)
+    
+    , 7: (f4,)
+    , 9: (f4,)
+    , 16: (f4,)
+    , 18: (f4,)
+    , 22: (f4,)
+    
+    , 3: (f5,)
+    , 15: (f5,)
+    , 19: (f5,)
+    , 23: (f5,)
+    , 24: (f5,)
+    }
