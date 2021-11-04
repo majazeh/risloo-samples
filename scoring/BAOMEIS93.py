866e72521ee01a3b3938b76e820a1afe829d835b
Mostafa-Alavi <mostafa.alavi25@gmail.com>
2021-11-04 19:34:59 +0330

    update BAOMEIS93 test scoring

--- scoring/BAOMEIS93.py
+++ scoring/BAOMEIS93.py
@@ -8,13 +8,11 @@ class BAOMEIS93(Data):
         option_numbers = dictionary.option_numbers
         score.set(dictionary.factors_names, 0)
     
-        score.set('raw' ,0)
         for i, item in self.items():   
             try:             
                 answer = int(item.get('user_answered')) 
                 factors = dictionary.factors[i + 1]
-                
-                score.increase('raw' , option_numbers + 1 - answer )                         
+                    
                 for factor in factors:    
                     score.increase(factor ,  option_numbers + 1 - answer) 
 
