694e83d0fb4a29099478aebac779186d0d641fe0
Mostafa Alavi <mostafa.alavi25@gmail.com>
2022-04-09 06:36:46 +0430

    add PSOC93 test

---
+++ scoring/dictionary/PSOC93.py
@@ -0,0 +1,29 @@
+f1 = 'satisfaction'
+f2 = 'efficiency'
+
+option_numbers = 6
+
+factors_names = (f1,f2)
+
+factors = { 
+    2 :(f1,)
+    , 3 :(f1,)
+    , 4 :(f1,)
+    , 5 :(f1,)
+    , 8 :(f1,) 
+    , 9 :(f1,)
+    , 12:(f1,)
+    , 14:(f1,)
+    , 16:(f1,)
+    
+    , 1:(f2,)
+    , 6:(f1,)
+    , 7:(f1,)
+    , 10:(f1,)
+    , 11:(f1,)
+    , 13:(f1,)
+    , 15:(f1,)
+}
+
+
+reverse_scoring_numbers =(1,6,7,10,11,13,15)
