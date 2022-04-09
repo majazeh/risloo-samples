8e8abebd6402d9ebb25fc4c61b219be7209ee480
Mostafa Alavi <mostafa.alavi25@gmail.com>
2022-04-09 06:36:31 +0430

    add CDRISC93 test

---
+++ scoring/CDRISC93.py
@@ -0,0 +1,18 @@
+from Data import Data
+import scoring.dictionary.CDRISC93 as dictionary
+
+class CDRISC93(Data):
+    scores = {'raw' :  None }# 
+    
+    def scoring_raw(self, score):
+        score.set(dictionary.factors_names,0)
+        for i, item in self.items():   
+            try:
+                answer = int(item.get('user_answered')) 
+                factors = dictionary.factors[i + 1]
+                for factor in factors:
+                    score.increase(factor , answer-1 ) 
+            except:
+                pass
+        
+     
---
+++ scoring/dictionary/CDRISC93.py
@@ -0,0 +1,42 @@
+f1 = 'personal_competence'
+f2 = 'trust_in_ones_instincts_and_tolerance_of_negative_affect'
+f3 = 'positive_acceptance_of_change_and_secure_relationship'
+f4 = 'control'
+f5 = 'spiritual_influences'
+
+factors_names = (f1,f2,f3,f4,f5)
+option_numbers = 5
+
+factors = {
+    10 :(f1,)
+    ,11 :(f1,)
+    ,12 :(f1,)
+    ,16 :(f1,)
+    ,17 :(f1,)
+    ,23 :(f1,)
+    ,24 :(f1,)
+    ,25 :(f1,)
+    
+    ,6 :(f2,)
+    ,7 :(f2,)
+    ,14 :(f2,)
+    ,15 :(f2,)
+    ,18 :(f2,)
+    ,19 :(f2,)
+    ,20 :(f2,)
+    
+    ,1 :(f3,)
+    ,2 :(f3,)
+    ,4 :(f3,)
+    ,5 :(f3,)
+    ,8 :(f3,)
+    
+    ,13 :(f4,)
+    ,21 :(f4,)
+    ,22 :(f4,)
+    
+    ,3 :(f5,)
+    ,9 :(f5,)
+    
+}
+
