9cb477868f644da41db0ec887a74494c21191b1d
abouhatef <itb.baravak@gmail.com>
2023-11-20 18:32:12 +0330

    JSIQ93 bugfix

--- scoring/JSIQ93.py
+++ scoring/JSIQ93.py
@@ -2,8 +2,8 @@ from Data import Data
 import scoring.dictionary.JSIQ93 as dictionary
 
 class JSIQ93(Data):
-    scores = {'raw' :  None }# 
-    
+    scores = {'raw' :  None }#
+
     def scoring_raw(self, score):
         total = 0
         section_1 = {"total": 0}
@@ -17,19 +17,22 @@ class JSIQ93(Data):
             section_1["total"] += answer
             total += answer
             section_1[key] = answer
-        
+
         for i,item in enumerate(list[30:], start=1):
             answer = int(item.get('user_answered'))
             answer = answer if answer is not None else 1
+            if i in dictionary.reverse_scoring_numbers:
+                answer += answer
+            else
+                answer += 6 - answer
             total += answer
             section_2["total"] += answer
             for f in dictionary.factors[i]:
                 section_2[f] += answer
-            
+
         score.set("total",total)
         score.set("section_1",section_1)
         score.set("section_2",section_2)
 
 
-        
-     
+
