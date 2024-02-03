033fd715316a19462a4d8daf8034944de7678ce7
abouhatef <itb.baravak@gmail.com>
2024-02-03 10:22:12 +0330

    YPI93

--- scoring/YPI93.py
+++ scoring/YPI93.py
@@ -6,31 +6,40 @@ class YPI93(Data):
  
     def scoring_raw(self, score): 
         iScore = {}
-        raw={
-                "father" : 0,
-                "mother" : 0,
-                "third" : 0,
-            }
-        all_items  = self.get_items_entirely()
 
+        all_items  = self.get_items_entirely()
+        fP = {}
+        for i in range(72):
+            factor = dictionary.items[i+1]
+            if factor in fP:
+                fP[factor] += 6
+            else: 
+                fP[factor] = 6
         for i in dictionary.factors:
             iScore[i] = {
-                "father" : 0,
-                "mother" : 0,
-                "third" : 0,
+                "father" : {
+                    "score": 0,
+                    "percentage": 0,
+                },
+                "mother" : {
+                    "score": 0,
+                    "percentage": 0,
+                },
             }
-        mod = ["father", "mother", "third"]
+        mod = ["father", "mother"]
         for i in range(72):
-            for j in range(3):
-                index = (i * 3) + j
+            for j in range(2):
+                index = (i * 2) + j
                 item = all_items[index]
                 answer = int(item.get('user_answered'))
                 factor = dictionary.items[i+1]
-                iScore[factor][mod[j]] += answer
-                raw[mod[j]] += answer
+                if factor == dictionary.f1:
+                    answer = 7 - answer
+                iScore[factor][mod[j]]["score"] += answer
         
         for i in dictionary.factors:
+            iScore[i]['father']["percentage"] = round(iScore[i]['father']["score"] / fP[i], 4)
+            iScore[i]['mother']["percentage"] = round(iScore[i]['mother']["score"] / fP[i], 4)
             score.set(i, iScore[i])
-        score.set('raw', raw)
             
             
