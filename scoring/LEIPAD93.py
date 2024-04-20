c3268c70cabe2d5fde6ee52a4d8c177e17022e36
abouhatef <itb.baravak@gmail.com>
2024-04-20 12:39:07 +0330

    LEIPAD

--- scoring/LEIPAD93.py
+++ scoring/LEIPAD93.py
@@ -5,7 +5,7 @@ class LEIPAD93(Data):
     scores = {'raw' :  None }# 
     
     def scoring_raw(self, score):
-        score.set(dictionary.factors_names,0)
+        score.set(dictionary.factors_names, 0)
         for i, item in self.items():   
             try:
                 answer = int(item.get('user_answered')) - 1
@@ -14,9 +14,10 @@ class LEIPAD93(Data):
                         answer = 1
                     else :
                         answer = 0
-                factors = dictionary.factors[i + 1]
-                for factor in factors:
-                    score.increase(factor , answer )    
+                factor = dictionary.factors[i + 1]
+                score.increase(factor , answer)
+                if i < 32 :
+                    score.increase('total_quality_of_life' , answer)
             except:
                 pass
         
