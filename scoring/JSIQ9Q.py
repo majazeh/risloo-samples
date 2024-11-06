f1a29b95bd447fc7135d76c7472ffcaf0882e618
abouhatef <itb.baravak@gmail.com>
2024-11-06 12:22:43 +0330

    bugfix

--- scoring/JSIQ9Q.py
+++ scoring/JSIQ9Q.py
@@ -22,8 +22,10 @@ class JSIQ9Q(Data):
         }
         for i, key in enumerate(dictionary.section_1_items):
             answer = int(list[i].get('user_answered'))
-            answer = (answer - 1) if answer is not None else 0
-            answer = 0 if answer < 0 else answer
+            if(i < 30):
+                answer = answer if answer is not None else 0
+            else:
+                answer = answer -1 if answer is not None else 0
             section_1[key] = answer
             subfactor = dictionary.section_1_factor[i + 1]
             factorL[subfactor] += 1
