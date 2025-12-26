0450cc5412243df10f3f34d0e647a23c3a015876
abouhatef <itb.baravak@gmail.com>
2025-12-26 10:34:07 +0330

    bugfix

--- scoring/YMQ93.py
+++ scoring/YMQ93.py
@@ -5,14 +5,29 @@ class YMQ93(Data):
     scores = {'raw' :  None }# 
     
     def scoring_raw(self, score):
+        factorList = {}
+        for factor in dictionary.factors_names:
+            factorList[factor] = {
+                "raw": 0,
+                "count": 0,
+            }
         score.set(dictionary.factors_names,0)
         for i, item in self.items():   
             try:
                 answer = int(item.get('user_answered')) 
                 factors = dictionary.factors[i + 1]
                 for factor in factors:
-                    score.increase(factor , answer ) if factor else None
+                    factorList[factor]['raw'] += answer
+                    factorList[factor]['count'] += 1
             except:
                 pass
+        print(factorList)
+        for factor in factorList:
+            score.set(factor, {
+                'raw': factorList[factor].get('raw'),
+                'mean': round(factorList[factor].get('raw') / factorList[factor].get('count'), 2),
+            })
+    
+    
         
      
